b3a9e4c7c4cc4e3dce7f2514c50ca7bd73dfa940
Margarita <mbiyun@bk.ru>
2022-12-14 14:05:24 +0700

    project

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿String[] Filter(String[] input) {
+    int maxSymbols = 3;
+    int count = 0;
+    for (int i = 0; i < input.Length; i++) {
+        if (input[i].Length <= maxSymbols) {
+            count++;
+        }
+    }
+    String[] output = new String[count];
+    int j = 0;
+    for (int i = 0; i < input.Length; i++) {
+        if (input[i].Length <= maxSymbols) {
+            output[j++] = input[i];
+        }
+    }
+    return output;
+}
+
+String[] input = new String[]{"hello", "2", ":-)"};
+String[] output = Filter(input);
+Console.WriteLine("[\"{0}\"] -> [\"{1}\"]", string.Join("\", \"", input), string.Join("\", \"", output));
+
+input = new String[]{"Russia", "Denmark", "Kazan"};
+output = Filter(input);
+Console.WriteLine("[\"{0}\"] -> [\"{1}\"]", string.Join("\", \"", input), string.Join("\", \"", output));
